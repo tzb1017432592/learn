ee86172cd966e3603558314f3a1c7ce9c875168c
tzb1017432592 <1017432592@qq.com>
2021-05-23 20:02:19 +0800

    111

---
+++ src/main/java/com/example/jianzhioffer/ShunShiZhiZhengDaYinJuZhen.java
@@ -0,0 +1,44 @@
+package com.example.jianzhioffer;
+
+/**
+ * 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。
+ *
+ * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[1,2,3,6,9,8,7,4,5]
+ *
+ *
+ * 输入：matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+ * 输出：[1,2,3,4,8,12,11,10,9,5,6,7]
+ */
+public class ShunShiZhiZhengDaYinJuZhen {
+    public int[] spiralOrder(int[][] matrix) {
+        if (matrix==null||matrix.length==0){
+            return new int[]{};
+        }
+        int r = matrix[0].length-1;
+        int b = matrix.length-1;
+        int l=0;
+        int t=0;
+        int index=0;
+        int[] res=new int[(r+1)*(b+1)];
+        while (r>=l){
+            for (int i=l;i<r-l;i++){
+                res[index++]=matrix[t][i];
+            }
+            for (int i=t;i<b-t;i++){
+                res[index++]=matrix[i][r];
+            }
+            for (int i=r;i>l;i--){
+                res[index++]=matrix[b][i];
+            }
+            for (int i=b;i>t;i--){
+                res[index++]=matrix[i][l];
+            }
+            l++;
+            r--;
+            t++;
+            b--;
+        }
+        return res;
+    }
+}
---
+++ src/main/java/com/example/jianzhioffer/XuanZhuangShuZu.java
@@ -0,0 +1,29 @@
+package com.example.jianzhioffer;
+
+/**
+ * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，
+ * 输出旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。  
+ * <p>
+ * 输入：[3,4,5,1,2]
+ * 输出：1
+ * <p>
+ * 输入：[2,2,2,0,1]
+ * 输出：0
+ */
+public class XuanZhuangShuZu {
+    public int minArray(int[] numbers) {
+        int r = numbers.length - 1;
+        int l = 0;
+        while (r > l) {
+            int m = (r + l) / 2;
+            if (numbers[m] > numbers[r]) {
+                l = m + 1;
+            } else if (numbers[m] < numbers[r]) {
+                r = m;
+            } else {
+                r--;
+            }
+        }
+        return numbers[l];
+    }
+}
