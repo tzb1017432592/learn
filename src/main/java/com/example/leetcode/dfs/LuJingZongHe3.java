a7d90766536b06e23185d6b36a66eb940d6275b4
tzb1017432592 <1017432592@qq.com>
2021-09-02 03:33:44 +0800

    111

---
+++ src/main/java/com/example/leetcode/dfs/LuJingZongHe3.java
@@ -0,0 +1,108 @@
+package com.example.leetcode.dfs;
+
+import java.util.*;
+
+/**
+ * 路径总和3
+ *
+ 给定一个二叉树的根节点 root ，和一个整数 targetSum ，求该二叉树里节点值之和等于 targetSum 的 路径 的数目。
+ 路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+
+ 示例 1：
+ 输入：root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+ 输出：3
+ 解释：和等于 8 的路径有 3 条，如图所示。
+
+ 示例 2：
+ 输入：root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+ 输出：3
+ *
+ */
+public class LuJingZongHe3 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    int res = 0;
+    public int pathSum(TreeNode root, int targetSum) {
+        loop(root,targetSum);
+        return res;
+    }
+    private void loop(TreeNode root,int targetSum){
+        if (root==null){
+            return;
+        }
+        dfs(root,targetSum);
+        loop(root.left,targetSum);
+        loop(root.right,targetSum);
+    }
+    private void dfs(TreeNode root, int targetSum) {
+        if (root==null){
+            return;
+        }
+        targetSum-=root.val;
+        if (targetSum==0){
+            res++;
+            return;
+        }
+        dfs(root.left,targetSum);
+        dfs(root.right,targetSum);
+    }
+
+
+    public int pathSum2(TreeNode root, int sum) {
+        // key是前缀和, value是大小为key的前缀和出现的次数
+        Map<Integer, Integer> prefixSumCount = new HashMap<>();
+        // 前缀和为0的一条路径
+        prefixSumCount.put(0, 1);
+        // 前缀和的递归回溯思路
+        return recursionPathSum(root, prefixSumCount, sum, 0);
+    }
+
+    /**
+     * 前缀和的递归回溯思路
+     * 从当前节点反推到根节点(反推比较好理解，正向其实也只有一条)，有且仅有一条路径，因为这是一棵树
+     * 如果此前有和为currSum-target,而当前的和又为currSum,两者的差就肯定为target了
+     * 所以前缀和对于当前路径来说是唯一的，当前记录的前缀和，在回溯结束，回到本层时去除，保证其不影响其他分支的结果
+     * @param node 树节点
+     * @param prefixSumCount 前缀和Map
+     * @param target 目标值
+     * @param currSum 当前路径和
+     * @return 满足题意的解
+     */
+    private int recursionPathSum(TreeNode node, Map<Integer, Integer> prefixSumCount, int target, int currSum) {
+        // 1.递归终止条件
+        if (node == null) {
+            return 0;
+        }
+        // 2.本层要做的事情
+        int res = 0;
+        // 当前路径上的和
+        currSum += node.val;
+
+        //---核心代码
+        // 看看root到当前节点这条路上是否存在节点前缀和加target为currSum的路径
+        // 当前节点->root节点反推，有且仅有一条路径，如果此前有和为currSum-target,而当前的和又为currSum,两者的差就肯定为target了
+        // currSum-target相当于找路径的起点，起点的sum+target=currSum，当前点到起点的距离就是target
+        res += prefixSumCount.getOrDefault(currSum - target, 0);
+        // 更新路径上当前节点前缀和的个数
+        prefixSumCount.put(currSum, prefixSumCount.getOrDefault(currSum, 0) + 1);
+        //---核心代码
+
+        // 3.进入下一层
+        res += recursionPathSum(node.left, prefixSumCount, target, currSum);
+        res += recursionPathSum(node.right, prefixSumCount, target, currSum);
+
+        // 4.回到本层，恢复状态，去除当前节点的前缀和数量
+        prefixSumCount.put(currSum, prefixSumCount.get(currSum) - 1);
+        return res;
+    }
+}
